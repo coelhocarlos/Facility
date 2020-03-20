ae87c10f97beb0c3ce09a47c047f8b862d76afc7
carlos coelho <carloscoelho_@msn.com>
2020-03-20 12:18:20 -0300

    Create mega.sh

---
+++ mega.sh
@@ -0,0 +1,7 @@
+
+    sudo apt install megatools wget
+    cd 
+    sudo touch ~/.megarc
+    echo "[Login]" >> ~/.megarc
+    echo "Username = facilitybackup2017@gmail.com" >> ~/.megarc
+    echo "Password = " >> ~/.megarc
